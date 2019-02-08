a72bdcbdafba4cf174212922b56e24502b9714ec
MingWai <cmwdes@gmail.com>
2019-02-08 12:58:58 +0100

    basic pipeline

---
+++ Assets/SRP0101_Basic/SRP0101.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SRP0101 : RenderPipelineAsset
+{
+    #if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0101", priority = 1)]
+    static void CreateSRP0101()
+    {
+        var instance = ScriptableObject.CreateInstance<SRP0101>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0101.asset");
+    }
+    #endif
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new SRP0101Instance();
+    }
+}
+
+public class SRP0101Instance : RenderPipeline
+{
+    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0101_Pass"); //The shader pass tag just for SRP0101
+
+    public SRP0101Instance()
+    {
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(cameras);
+
+        foreach (Camera camera in cameras)
+        {
+            BeginCameraRendering(camera);
+
+            //Culling
+            ScriptableCullingParameters cullingParams;
+            if (!camera.TryGetCullingParameters(out cullingParams))
+                continue;
+            CullingResults cull = context.Cull(ref cullingParams);
+
+            //Camera setup some builtin variables e.g. camera projection matrices etc
+            context.SetupCameraProperties(camera);
+
+            //Get the setting from camera component
+            bool drawSkyBox = camera.clearFlags == CameraClearFlags.Skybox? true : false;
+            bool clearDepth = camera.clearFlags == CameraClearFlags.Nothing? false : true;
+            bool clearColor = camera.clearFlags == CameraClearFlags.Color? true : false;
+
+            //Camera clear flag
+            CommandBuffer cmd = new CommandBuffer();
+            cmd.ClearRenderTarget(clearDepth, clearColor, camera.backgroundColor);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Release();
+
+            //Setup DrawSettings and FilterSettings
+            var sortingSettings = new SortingSettings(camera);
+            DrawingSettings drawSettings = new DrawingSettings(m_PassName, sortingSettings);
+            FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
+
+            //Skybox
+            if(drawSkyBox)  {  context.DrawSkybox(camera);  }
+
+            //Opaque objects
+            sortingSettings.criteria = SortingCriteria.CommonOpaque;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            //Transparent objects
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            context.Submit();
+        }
+    }
+}
