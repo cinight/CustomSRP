8195246821bc80184caa39dc783b9733eb8e3468
CMW <cmwdes@gmail.com>
2019-02-09 23:40:33 +0100

    ugui scene

--- Assets/SRP0801_UGUI/SRP0801.cs
+++ Assets/SRP0801_UGUI/SRP0801.cs
@@ -3,29 +3,29 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 [ExecuteInEditMode]
-public class SRP0401 : RenderPipelineAsset
+public class SRP0801 : RenderPipelineAsset
 {
     #if UNITY_EDITOR
-    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0401", priority = 1)]
-    static void CreateSRP0401()
+    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0801", priority = 1)]
+    static void CreateSRP0801()
     {
-        var instance = ScriptableObject.CreateInstance<SRP0401>();
-        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0401.asset");
+        var instance = ScriptableObject.CreateInstance<SRP0801>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0801.asset");
     }
     #endif
 
     protected override RenderPipeline CreatePipeline()
     {
-        return new SRP0401Instance();
+        return new SRP0801Instance();
     }
 }
 
-public class SRP0401Instance : RenderPipeline
+public class SRP0801Instance : RenderPipeline
 {
-    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0401_Pass"); //The shader pass tag just for SRP0401
+    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0801_Pass"); //The shader pass tag just for SRP0801
     private static readonly ShaderTagId m_PassNameDefault = new ShaderTagId("SRPDefaultUnlit"); //The shader pass tag for replacing shaders without pass
 
-    public SRP0401Instance()
+    public SRP0801Instance()
     {
     }
 
@@ -35,6 +35,11 @@ public class SRP0401Instance : RenderPipeline
 
         foreach (Camera camera in cameras)
         {
+            #if UNITY_EDITOR
+            bool isSceneViewCam = camera.cameraType == CameraType.SceneView;
+            if(isSceneViewCam) ScriptableRenderContext.EmitWorldGeometryForSceneView(camera); //This makes the UI Canvas geometry appear on scene view
+            #endif
+
             BeginCameraRendering(camera);
 
             //Culling
@@ -60,8 +65,10 @@ public class SRP0401Instance : RenderPipeline
             //Setup DrawSettings and FilterSettings
             var sortingSettings = new SortingSettings(camera);
             DrawingSettings drawSettings = new DrawingSettings(m_PassName, sortingSettings);
-            DrawingSettings drawSettingsDefault = new DrawingSettings(m_PassNameDefault, sortingSettings);
-                            drawSettingsDefault.SetShaderPassName(1,m_PassNameDefault); //This will let you draw shader passes without the LightMode
+           DrawingSettings drawSettingsDefault = new DrawingSettings(m_PassNameDefault, sortingSettings);
+            //This will let you draw shader passes without the LightMode,
+            //thus it draws the default UGUI materials
+            drawSettingsDefault.SetShaderPassName(1,m_PassNameDefault); 
             FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
 
             //Skybox
@@ -82,6 +89,14 @@ public class SRP0401Instance : RenderPipeline
 
             //Transparent default
             context.DrawRenderers(cull, ref drawSettingsDefault, ref filterSettings);
+
+            //SceneView fix, so that it draws the gizmos on scene view
+            #if UNITY_EDITOR
+            if (isSceneViewCam)
+            {
+                context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+            }
+            #endif
 
             context.Submit();
         }
