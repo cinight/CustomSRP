6c193dbb4a7288668eee432e6611a9c44d6f1278
CMW <cmwdes@gmail.com>
2019-02-16 19:33:28 +0100

    soft particle

---
+++ Assets/SRP0501_SoftParticle/SRP0501.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SRP0501 : RenderPipelineAsset
+{
+    #if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0501", priority = 1)]
+    static void CreateSRP0501()
+    {
+        var instance = ScriptableObject.CreateInstance<SRP0501>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0501.asset");
+    }
+    #endif
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new SRP0501Instance();
+    }
+}
+
+public class SRP0501Instance : RenderPipeline
+{
+    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0501_Pass"); //The shader pass tag just for SRP0501
+
+    private static Material depthOnlyMaterial;
+    private static int m_DepthRTid = Shader.PropertyToID("_CameraDepthTexture");
+    private static RenderTargetIdentifier m_DepthRT = new RenderTargetIdentifier(m_DepthRTid);
+    private int depthBufferBits = 24;
+
+    public SRP0501Instance()
+    {
+        depthOnlyMaterial = new Material(Shader.Find("Hidden/CustomSRP/SRP0702/DepthOnly"));
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(cameras);
+
+        foreach (Camera camera in cameras)
+        {
+            BeginCameraRendering(camera);
+
+            //Culling
+            ScriptableCullingParameters cullingParams;
+            if (!camera.TryGetCullingParameters(out cullingParams))
+                continue;
+            CullingResults cull = context.Cull(ref cullingParams);
+
+            //Camera setup some builtin variables e.g. camera projection matrices etc
+            context.SetupCameraProperties(camera);
+
+            //Get the setting from camera component
+            bool drawSkyBox = camera.clearFlags == CameraClearFlags.Skybox? true : false;
+            bool clearDepth = camera.clearFlags == CameraClearFlags.Nothing? false : true;
+            bool clearColor = camera.clearFlags == CameraClearFlags.Color? true : false;
+
+            //Set Depth texture temp RT
+            CommandBuffer cmdTempId = new CommandBuffer();
+            cmdTempId.name = "("+camera.name+")"+ "Setup TempRT";
+            RenderTextureDescriptor depthRTDesc = new RenderTextureDescriptor(camera.pixelWidth, camera.pixelHeight);
+                depthRTDesc.colorFormat = RenderTextureFormat.Depth;
+                depthRTDesc.depthBufferBits = depthBufferBits;
+                cmdTempId.GetTemporaryRT(m_DepthRTid, depthRTDesc,FilterMode.Bilinear);
+            context.ExecuteCommandBuffer(cmdTempId);
+            cmdTempId.Release();
+
+            //Setup DrawSettings and FilterSettings
+            var sortingSettings = new SortingSettings(camera);
+            DrawingSettings drawSettings = new DrawingSettings(m_PassName, sortingSettings);
+            FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
+            DrawingSettings drawSettingsDepth = new DrawingSettings(m_PassName, sortingSettings)
+            {
+                perObjectData = PerObjectData.None,
+                overrideMaterial = depthOnlyMaterial,
+                overrideMaterialPassIndex = 0
+            };
+
+            //Clear Depth Texture
+            CommandBuffer cmdDepth = new CommandBuffer();
+            cmdDepth.name = "("+camera.name+")"+ "Depth Clear Flag";
+            cmdDepth.SetRenderTarget(m_DepthRT); //Set CameraTarget to the depth texture
+            cmdDepth.ClearRenderTarget(true, true, Color.black);
+            context.ExecuteCommandBuffer(cmdDepth);
+            cmdDepth.Release();
+
+            //Draw Depth with Opaque objects
+            sortingSettings.criteria = SortingCriteria.CommonOpaque;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull, ref drawSettingsDepth, ref filterSettings);
+
+            //To let shader has _CameraDepthTexture
+            CommandBuffer cmdDepthTexture = new CommandBuffer();
+            cmdDepthTexture.name = "("+camera.name+")"+ "Depth Texture";
+            cmdDepthTexture.SetGlobalTexture(m_DepthRTid,m_DepthRT);
+            context.ExecuteCommandBuffer(cmdDepthTexture);
+            cmdDepthTexture.Release();
+
+            //Camera clear flag
+            CommandBuffer cmd = new CommandBuffer();
+            cmd.SetRenderTarget(BuiltinRenderTextureType.CameraTarget); //Rember to reset target
+            cmd.ClearRenderTarget(clearDepth, clearColor, camera.backgroundColor);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Release();
+
+            //Skybox
+            if(drawSkyBox)  {  context.DrawSkybox(camera);  }
+
+            //Opaque objects
+            sortingSettings.criteria = SortingCriteria.CommonOpaque;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            //Transparent objects
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            context.Submit();
+        }
+    }
+}
---
+++ Assets/SRP0501_SoftParticle/SRP0501_SoftParticle.shader
@@ -0,0 +1,92 @@
+﻿Shader "CustomSRP/SRP0501/SoftParticle"
+{
+    Properties 
+    {
+        _MainTex ("Particle Texture", 2D) = "white" {}
+        _InvFade ("Soft Particles Factor", Range(0.01,3.0)) = 1.0
+
+        _EdgeAroundColor("Edge Color", Color) = (1,1,1,1)
+        _EdgeAroundPower("Edge Color Power",Range(1,10)) = 1
+    }
+
+    SubShader 
+    {
+        Tags { "Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent" }
+        Blend SrcAlpha OneMinusSrcAlpha
+        Cull Back Lighting Off ZWrite On //Set to on, so that the later transparent objects will render correctly
+
+        Pass 
+        {
+            Tags { "LightMode" = "SRP0501_Pass" }
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+            #pragma multi_compile __ _FLIPUV
+
+            #include "UnityCG.cginc"
+
+
+            struct appdata_t 
+            {
+                float4 vertex : POSITION;
+                float2 texcoord : TEXCOORD0;
+            };
+
+            struct v2f 
+            {
+                float4 vertex : SV_POSITION;
+                float2 texcoord : TEXCOORD0;
+                float4 projPos : TEXCOORD2;
+            };
+
+            sampler2D _CameraDepthTexture;
+            float _InvFade;
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+       
+            fixed4 _TintColor;
+			fixed4 _EdgeAroundColor;
+			fixed _EdgeAroundPower;
+
+            v2f vert (appdata_t v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.projPos = ComputeScreenPos (o.vertex);
+
+                COMPUTE_EYEDEPTH(o.projPos.z);
+                o.texcoord = v.texcoord;
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				half4 col = tex2D(_MainTex, i.texcoord);
+
+                    float2 uv = i.projPos.xy/ i.projPos.z;
+
+                    //#if defined(UNITY_UV_STARTS_AT_TOP) && !defined(_FLIPUV)
+                    //    uv.y = 1- uv.y;
+                    //#endif
+
+                    float sceneZ = LinearEyeDepth (tex2D(_CameraDepthTexture, uv));
+                    float partZ = i.projPos.z;
+                    float fZ = (sceneZ-partZ);
+                    float fade = saturate (_InvFade * fZ);
+                    col.a *= fade;
+
+                    float edgearound = pow( fade *_EdgeAroundColor.a, _EdgeAroundPower);
+                    col.rgb = lerp( _EdgeAroundColor.rgb, col.rgb, edgearound);
+
+                    //float depth = tex2D(_CameraDepthTexture, i.texcoord).r * 10;
+                    //float4 c = float4(depth, 0 ,0,1);
+
+                return col*0.8f;
+            }
+            ENDCG
+        }
+    }
+}
