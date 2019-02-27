d3693db451ff133d543c8e7c0d988bdce6e8e7dc
Ming Wai Chan <cmwdes@gmail.com>
2019-02-27 21:01:49 +0100

    callback

---
+++ Assets/SRP0405_Callback/CallbackTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CallbackTest : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+
+    void OnEnable()
+    {
+        SRP0405Instance.afterSkybox += MyAfterSkybox;
+        SRP0405Instance.afterOpaqueObject += MyAfterOpaque;
+        SRP0405Instance.afterTransparentObject += MyAfterTransparent;
+    }
+
+    void OnDisable()
+    {
+        CleanUp();
+    }
+
+    void OnDestroy()
+    {
+        CleanUp();
+    }
+
+    private void CleanUp()
+    {
+        SRP0405Instance.afterSkybox -= MyAfterSkybox;
+        SRP0405Instance.afterOpaqueObject -= MyAfterOpaque;
+        SRP0405Instance.afterTransparentObject -= MyAfterTransparent;
+    }
+
+    private void MyAfterSkybox(Camera cam, ScriptableRenderContext context)
+    {
+        //Debug.Log("after skybox is called");
+    }
+
+    private void MyAfterOpaque(Camera cam, ScriptableRenderContext context)
+    {
+        //Debug.Log("after opaque is called");
+    }
+
+    private void MyAfterTransparent(Camera cam, ScriptableRenderContext context)
+    {
+        //Debug.Log("after transparent is called");
+        CommandBuffer cmd = new CommandBuffer();
+        cmd.DrawMesh(mesh,Matrix4x4.identity,material,0,0);
+        context.ExecuteCommandBuffer(cmd);
+    }
+}
---
+++ Assets/SRP0405_Callback/SRP0405.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SRP0405 : RenderPipelineAsset
+{
+    #if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0405", priority = 1)]
+    static void CreateSRP0405()
+    {
+        var instance = ScriptableObject.CreateInstance<SRP0405>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0405.asset");
+    }
+    #endif
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new SRP0405Instance();
+    }
+}
+
+public class SRP0405Instance : RenderPipeline
+{
+    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0405_Pass"); //The shader pass tag just for SRP0405
+
+    //Custom callbacks
+    public static event Action<Camera,ScriptableRenderContext> afterSkybox;
+    public static event Action<Camera,ScriptableRenderContext> afterOpaqueObject;
+    public static event Action<Camera,ScriptableRenderContext> afterTransparentObject;
+
+    public SRP0405Instance()
+    {
+    }
+
+    public static void AfterSkybox(Camera camera, ScriptableRenderContext context)
+    {
+        afterSkybox?.Invoke(camera,context);
+    }
+
+    public static void AfterOpaqueObject(Camera camera, ScriptableRenderContext context)
+    {
+        afterOpaqueObject?.Invoke(camera,context);
+    }
+
+    public static void AfterTransparentObject(Camera camera, ScriptableRenderContext context)
+    {
+        afterTransparentObject?.Invoke(camera,context);
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(cameras);
+
+        foreach (Camera camera in cameras)
+        {
+            BeginCameraRendering(camera);
+
+            //Culling
+            ScriptableCullingParameters cullingParams;
+            if (!camera.TryGetCullingParameters(out cullingParams))
+                continue;
+            CullingResults cull = context.Cull(ref cullingParams);
+
+            //Camera setup some builtin variables e.g. camera projection matrices etc
+            context.SetupCameraProperties(camera);
+
+            //Get the setting from camera component
+            bool drawSkyBox = camera.clearFlags == CameraClearFlags.Skybox? true : false;
+            bool clearDepth = camera.clearFlags == CameraClearFlags.Nothing? false : true;
+            bool clearColor = camera.clearFlags == CameraClearFlags.Color? true : false;
+
+            //Camera clear flag
+            CommandBuffer cmd = new CommandBuffer();
+            cmd.ClearRenderTarget(clearDepth, clearColor, camera.backgroundColor);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Release();
+
+            //Setup DrawSettings and FilterSettings
+            var sortingSettings = new SortingSettings(camera);
+            DrawingSettings drawSettings = new DrawingSettings(m_PassName, sortingSettings);
+            FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
+
+            //Skybox
+            if(drawSkyBox)  {  context.DrawSkybox(camera);  }
+
+            //Callback
+            AfterSkybox(camera,context);
+
+            //Opaque objects
+            sortingSettings.criteria = SortingCriteria.CommonOpaque;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            //Callback
+            AfterOpaqueObject(camera,context);
+
+            //Transparent objects
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            //Callback
+            AfterTransparentObject(camera,context);
+
+            context.Submit();
+
+            EndCameraRendering(context,camera);
+        }
+
+        EndFrameRendering(context,cameras);
+    }
+}
