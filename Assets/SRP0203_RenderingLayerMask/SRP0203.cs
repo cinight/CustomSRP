6f891c98ae44831d0196bed781eb9392b63999bf
MingWai <cmwdes@gmail.com>
2019-06-13 11:11:30 +0200

    RenderingLayerMask scene

---
+++ Assets/SRP0203_RenderingLayerMask/SRP0203.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SRP0203 : RenderPipelineAsset
+{
+    #if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/Render Pipeline/SRP0203", priority = 1)]
+    static void CreateSRP0203()
+    {
+        var instance = ScriptableObject.CreateInstance<SRP0203>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/SRP0203.asset");
+    }
+    #endif
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new SRP0203Instance();
+    }
+}
+
+public class SRP0203Instance : RenderPipeline
+{
+    private static readonly ShaderTagId m_PassName = new ShaderTagId("SRP0203_Pass"); //The shader pass tag just for SRP0203
+
+    public SRP0203Instance()
+    {
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(cameras);
+
+        foreach (Camera camera in cameras)
+        {
+            BeginCameraRendering(camera);
+
+            //Culling
+            ScriptableCullingParameters cullingParams;
+            if (!camera.TryGetCullingParameters(out cullingParams))
+                continue;
+            CullingResults cull = context.Cull(ref cullingParams);
+
+            //Camera setup some builtin variables e.g. camera projection matrices etc
+            context.SetupCameraProperties(camera);
+
+            //Get the setting from camera component
+            bool drawSkyBox = camera.clearFlags == CameraClearFlags.Skybox? true : false;
+            bool clearDepth = camera.clearFlags == CameraClearFlags.Nothing? false : true;
+            bool clearColor = camera.clearFlags == CameraClearFlags.Color? true : false;
+
+            //Camera clear flag
+            CommandBuffer cmd = new CommandBuffer();
+            cmd.ClearRenderTarget(clearDepth, clearColor, camera.backgroundColor);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Release();
+
+            //Setup DrawSettings and FilterSettings
+            var sortingSettings = new SortingSettings(camera);
+            DrawingSettings drawSettings = new DrawingSettings(m_PassName, sortingSettings);
+            FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
+            //filterSettings.layerMask = 1; //only draws objects marked with UI layer
+            filterSettings.renderingLayerMask = 1 << 4; //only draws renderers that has RenderingLayerMask Layer5 **********************************
+
+            //Skybox
+            if(drawSkyBox)  {  context.DrawSkybox(camera);  }
+
+            //Opaque objects
+            sortingSettings.criteria = SortingCriteria.CommonOpaque;
+            drawSettings.sortingSettings = sortingSettings;
+            filterSettings.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            //Transparent objects
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            drawSettings.sortingSettings = sortingSettings;
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
+
+            context.Submit();
+        }
+    }
+}
